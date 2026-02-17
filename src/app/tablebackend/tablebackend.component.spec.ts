3f06bf0914b862a19bc2cd7bdf2fdfe8a0eeee3d
FiatTy <zxiuy123@gmail.com>
2026-02-17 10:45:08 +0700

    Update Karma Coverage Test

--- src/app/tablebackend/tablebackend.component.spec.ts
+++ src/app/tablebackend/tablebackend.component.spec.ts
@@ -8,9 +8,9 @@ describe('TablebackendComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TablebackendComponent ]
+      declarations: [TablebackendComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
